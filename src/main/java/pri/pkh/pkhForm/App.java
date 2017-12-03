58eda0d2e768d36080c63674a02a16c35c0784ec
YimJunHyeok <flamemaple79@gmail.com>
2017-12-03 14:54:04 +0900

    first commit

---
+++ src/main/java/pri/pkh/pkhForm/App.java
@@ -0,0 +1,13 @@
+package pri.pkh.pkhForm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
